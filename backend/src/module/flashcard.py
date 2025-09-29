27f9e3d8a14183c563e43ca0d66b72aa53480d0e
stanleyyoga123 <yoga360227@gmail.com>
2025-09-29 17:54:43 +1000

    feat: add flashcard module

---
+++ backend/src/module/flashcard.py
@@ -0,0 +1,23 @@
+from google import genai
+
+from common.constant import GENERATOR_MODEL
+from common.prompt import FLASHCARD_CREATION_PROMPT
+from common.utility import parse_response_to_json
+
+
+class FlashcardModule:
+    def __init__(self):
+        self._client = genai.Client()
+
+    def create_flashcard(
+        self, num_flashcards: int, general_idea: str, topics: list[str]
+    ) -> dict[str, any]:
+        content = FLASHCARD_CREATION_PROMPT.format(
+            num_flashcards=num_flashcards,
+            general_idea=general_idea,
+            topics=" ; ".join(topics),
+        )
+        response = self._client.models.generate_content(
+            model=GENERATOR_MODEL, contents=content
+        )
+        return parse_response_to_json(response.text)
